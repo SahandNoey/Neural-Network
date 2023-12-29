c8d4d539234620a6d670d4fb2778320e8cd8053b
SahandNoey <sahandnoey7798@gmail.com>
2023-12-29 19:26:47 +0330

    Loss functions implemented

--- neuralNetworks/rsdl/losses/loss_functions.py
+++ neuralNetworks/rsdl/losses/loss_functions.py
@@ -1,12 +1,23 @@
 from rsdl import Tensor
 
-def MeanSquaredError(preds: Tensor, actual: Tensor):
+
+def MeanSquaredError(predicts: Tensor, actual: Tensor):
     # TODO : implement mean squared error
-    return None
+    error = actual - predicts
+    return (error ** 2).sum() * (Tensor(1 / len(actual.data), requires_grad=actual.requires_grad,
+                                        depends_on=actual.depends_on))
 
-def CategoricalCrossEntropy(preds: Tensor, actual: Tensor):
-    # TODO : imlement categorical cross entropy 
-    return None
 
+def CategoricalCrossEntropy(predicts: Tensor, actual: Tensor):
+    # TODO : implement categorical cross entropy
+    sum_exps = predicts.exp().sum()
+
+    softmax = sum_exps * Tensor(data=(1 / sum_exps.data), requires_grad=sum_exps.requires_grad,
+                                depends_on=sum_exps.depends_on)
+    log_softmax = softmax.log()
+
+    l_vector = -actual * log_softmax
+    l = -l_vector.sum()
+    return l
 
 

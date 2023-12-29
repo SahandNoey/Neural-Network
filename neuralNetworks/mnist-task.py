4d665d347a3cc65c07d4d726bb69af2d5fc12af7
SahandNoey <sahandnoey7798@gmail.com>
2023-12-30 00:41:07 +0330

    task6 implemented but has bugs

--- neuralNetworks/mnist-task.py
+++ neuralNetworks/mnist-task.py
@@ -7,60 +7,51 @@ import numpy as np
 from rsdl.layers import Linear
 from rsdl.activations import Relu
 from rsdl.optim import SGD
-from rsdl.optim import Adam
 from rsdl.tensors import Tensor
 from rsdl.losses import CategoricalCrossEntropy
-from rsdl.losses import MeanSquaredError
-import time
 import sys
-import pickle
 
-sys.setrecursionlimit(1000000)
+sys.setrecursionlimit(10**8)
 
-# loading the data
-train_data = pd.read_csv("./MNIST_CSV/mnist_train.csv", sep=',', header=None)
+train_data = pd.read_csv(
+    "/mnt/d/Projects/PythonProjects/computationalIntelligence/neuralNetworks/MNIST_CSV/mnist_train.csv",
+    sep=',', header=None)
 train_label = train_data.iloc[:, 0]
 train_data = train_data.iloc[:, 1:]
 
 train_data = train_data.to_numpy()
 train_label = train_label.to_numpy()
 
-test_data = pd.read_csv("./MNIST_CSV/mnist_test.csv", sep=',', header=None)
+test_data = pd.read_csv(
+    "/mnt/d/Projects/PythonProjects/computationalIntelligence/neuralNetworks/MNIST_CSV/mnist_test.csv",
+    sep=',', header=None)
+
 test_label = test_data.iloc[:, 0]
 test_data = test_data.iloc[:, 1:]
 test_data = test_data.to_numpy()
 test_label = test_label.to_numpy()
 
-# now converting to out Tensor class
 train_data = Tensor(data=train_data)
 train_label = Tensor(data=train_label)
 test_data = Tensor(data=test_data)
 test_label = Tensor(data=test_label)
 
-# now normalizing the data
-# normalize(train_data)
-# normalize(test_data)
+train_batch_size = 32
+test_batch_size = 64
+hidden_neuron_num = 50
 
-# now building the model
-hidden_neuron_num = 100
 model = {"layers": [
-    Linear(784, hidden_neuron_num),
-    Linear(hidden_neuron_num, 10)
-],
-    "activations": [
-        Relu,
-    ]
-}
+    Linear(28*28, hidden_neuron_num),
+    Linear(hidden_neuron_num, 10)], "activations": [Relu]}
 
-optimizer = SGD(layers=model["layers"], learning_rate=0.1)
-batch_size = 32
+optimizer = SGD(layers=model["layers"], lr=1)
 
 for epoch in range(20):
     epoch_loss = 0.0
     print(f"epoch is: {epoch}")
-
-    for start in range(0, train_data.shape[0], batch_size):
-        end = start + batch_size
+    print(f"epoch loss = {epoch_loss}")
+    for start in range(0, train_data.shape[0], train_batch_size):
+        end = start + train_batch_size
 
         inputs = train_data[start:end]
 
@@ -68,35 +59,27 @@ for epoch in range(20):
         for layer in model["layers"]:
             layer.zero_grad()
 
-        # forward phase
-        a1_raw = model["layers"][0](inputs)
-        a1 = model["activations"][0](a1_raw)
-        predicted = model["layers"][1](a1)
+        hidden_input_data = model["layers"][0](inputs)
+        hidden_output_data = model["activations"][0](hidden_input_data)
+        output_data = model["layers"][1](hidden_output_data)
 
         actual = train_label[start:end]
         actual.data = np.eye(10)[actual.data.tolist()]
 
-        loss = MeanSquaredError(predicted, actual)
+        loss = CategoricalCrossEntropy(output_data, actual)
         loss.backward()
 
         epoch_loss += loss
 
         optimizer.step()
-        print("loss:", loss)
 
-    print("epoch ended")
-    # now evaluation
-    print("now, evaluating...")
-    for start in range(0, test_data.shape[0], batch_size):
-        end = start + batch_size
+    for start in range(0, test_data.shape[0], test_batch_size):
+        end = start + test_batch_size
         inputs = test_data[start:end]
 
-        a1_raw = model["layers"][0](inputs)
-        a1 = model["activations"][0](a1_raw)
-        predicted = model["layers"][1](a1)
+        hidden_input_data = model["layers"][0](inputs)
+        hidden_output_data = model["activations"][0](hidden_input_data)
+        output_data = model["layers"][1](hidden_output_data)
 
-        predicted = np.argmax(predicted, axis=1)
-        predicted = predicted.reshape(-1, 1)
-        print("predicted is:", predicted)
-
-print("now here")
+        output_data = np.argmax(output_data, axis=1)
+        output_data = output_data.reshape(-1, 1)
--- neuralNetworks/rsdl/losses/loss_functions.py
+++ neuralNetworks/rsdl/losses/loss_functions.py
@@ -1,23 +1,15 @@
 from rsdl import Tensor
+from rsdl.activations import Softmax
+from rsdl.tensors import _tensor_neg
 
-
-def MeanSquaredError(predicts: Tensor, actual: Tensor):
+def MeanSquaredError(preds: Tensor, actual: Tensor) -> Tensor:
     # TODO : implement mean squared error
-    error = actual - predicts
-    return (error ** 2).sum() * (Tensor(1 / len(actual.data), requires_grad=actual.requires_grad,
-                                        depends_on=actual.depends_on))
+    err = actual - preds
+    return (err ** 2).sum() * (Tensor(1 / len(actual.data), requires_grad=actual.requires_grad, depends_on=actual.depends_on))
 
-
-def CategoricalCrossEntropy(predicts: Tensor, actual: Tensor):
-    # TODO : implement categorical cross entropy
-    sum_exps = predicts.exp().sum()
-
-    softmax = sum_exps * Tensor(data=(1 / sum_exps.data), requires_grad=sum_exps.requires_grad,
-                                depends_on=sum_exps.depends_on)
-    log_softmax = softmax.log()
-
-    l_vector = -actual * log_softmax
-    return -l_vector.sum()
-
-
+def CategoricalCrossEntropy(preds: Tensor, actual: Tensor) -> Tensor:
+    # TODO : imlement categorical cross entropy
+    preds = Softmax(preds)
+    log_preds = preds.log()
+    return _tensor_neg((actual * log_preds).sum())
 

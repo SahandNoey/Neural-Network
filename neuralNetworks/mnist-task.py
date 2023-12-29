3c85a3d00763bc806ee74cb3f7ac40cc1a2c623c
SahandNoey <sahandnoey7798@gmail.com>
2023-12-29 23:28:31 +0330

    activation functions implemented

--- neuralNetworks/mnist-task.py
+++ neuralNetworks/mnist-task.py
@@ -2,3 +2,101 @@
 # 1. load mnist dataset for our framework
 # 2. define your model
 # 3. start training and have fun!
+import pandas as pd
+import numpy as np
+from rsdl.layers import Linear
+from rsdl.activations import Relu
+from rsdl.optim import SGD
+from rsdl.optim import Adam
+from rsdl.tensors import Tensor
+from rsdl.losses import CategoricalCrossEntropy
+from rsdl.losses import MeanSquaredError
+import time
+import sys
+import pickle
+
+sys.setrecursionlimit(1000000)
+
+# loading the data
+train_data = pd.read_csv("./MNIST_CSV/mnist_train.csv", sep=',', header=None)
+train_label = train_data.iloc[:, 0]
+train_data = train_data.iloc[:, 1:]
+
+train_data = train_data.to_numpy()
+train_label = train_label.to_numpy()
+
+test_data = pd.read_csv("./MNIST_CSV/mnist_test.csv", sep=',', header=None)
+test_label = test_data.iloc[:, 0]
+test_data = test_data.iloc[:, 1:]
+test_data = test_data.to_numpy()
+test_label = test_label.to_numpy()
+
+# now converting to out Tensor class
+train_data = Tensor(data=train_data)
+train_label = Tensor(data=train_label)
+test_data = Tensor(data=test_data)
+test_label = Tensor(data=test_label)
+
+# now normalizing the data
+# normalize(train_data)
+# normalize(test_data)
+
+# now building the model
+hidden_neuron_num = 100
+model = {"layers": [
+    Linear(784, hidden_neuron_num),
+    Linear(hidden_neuron_num, 10)
+],
+    "activations": [
+        Relu,
+    ]
+}
+
+optimizer = SGD(layers=model["layers"], learning_rate=0.1)
+batch_size = 32
+
+for epoch in range(20):
+    epoch_loss = 0.0
+    print(f"epoch is: {epoch}")
+
+    for start in range(0, train_data.shape[0], batch_size):
+        end = start + batch_size
+
+        inputs = train_data[start:end]
+
+        inputs.zero_grad()
+        for layer in model["layers"]:
+            layer.zero_grad()
+
+        # forward phase
+        a1_raw = model["layers"][0](inputs)
+        a1 = model["activations"][0](a1_raw)
+        predicted = model["layers"][1](a1)
+
+        actual = train_label[start:end]
+        actual.data = np.eye(10)[actual.data.tolist()]
+
+        loss = MeanSquaredError(predicted, actual)
+        loss.backward()
+
+        epoch_loss += loss
+
+        optimizer.step()
+        print("loss:", loss)
+
+    print("epoch ended")
+    # now evaluation
+    print("now, evaluating...")
+    for start in range(0, test_data.shape[0], batch_size):
+        end = start + batch_size
+        inputs = test_data[start:end]
+
+        a1_raw = model["layers"][0](inputs)
+        a1 = model["activations"][0](a1_raw)
+        predicted = model["layers"][1](a1)
+
+        predicted = np.argmax(predicted, axis=1)
+        predicted = predicted.reshape(-1, 1)
+        print("predicted is:", predicted)
+
+print("now here")
--- neuralNetworks/rsdl/activations/activation_functions.py
+++ neuralNetworks/rsdl/activations/activation_functions.py
@@ -1,15 +1,25 @@
 from rsdl import Tensor, Dependency
 import numpy as np
+import rsdl.tensors as rs
+
 
 def Sigmoid(t: Tensor) -> Tensor:
     # TODO: implement sigmoid function
     # hint: you can do it using function you've implemented (not directly define grad func)
-    return None
+    neg_exp = rs._tensor_neg(t).exp()
+    denominator = neg_exp + 1.0
+    return Tensor(data=(1 / denominator), requires_grad=denominator.requires_grad, depends_on=denominator.depends_on)
+
 
 def Tanh(t: Tensor) -> Tensor:
     # TODO: implement tanh function
     # hint: you can do it using function you've implemented (not directly define grad func)
-    return None
+    exp_matrix = t.exp()
+    neg_exp_matrix = rs._tensor_neg(t).exp()
+    numerator = exp_matrix - neg_exp_matrix
+    denominator = exp_matrix + neg_exp_matrix
+    return numerator * Tensor(data=(1 / denominator.data), requires_grad=denominator.requires_grad, depends_on=denominator.depends_on)
+
 
 def Softmax(t: Tensor) -> Tensor:
     # TODO: implement softmax function
@@ -18,40 +28,45 @@ def Softmax(t: Tensor) -> Tensor:
     #        1. implement sum by axis
     #        2. using matrix mul to do it :) (recommended)
     # hint: a/b = a*(b^-1)
-    return None
+
+    exp_matrix = t.exp()
+    sum = exp_matrix @ Tensor(data=np.ones((exp_matrix.data.shape[1], 1)), requires_grad=True)
+    sum_invert = Tensor(data=(1 / sum.data), requires_grad=sum.requires_grad, depends_on=sum.depends_on)
+    return exp_matrix * sum_invert
+
 
 def Relu(t: Tensor) -> Tensor:
     # TODO: implement relu function
 
     # use np.maximum
-    data = ...
+    data = np.maximum(0, t.data)
+    req_grad = t.requires_grad
 
-    req_grad = ...
     if req_grad:
         def grad_fn(grad: np.ndarray):
             # use np.where
-            return None
-        
+            return np.where(t.data > 0, grad, 0)
+
         depends_on = [Dependency(t, grad_fn)]
     else:
         depends_on = []
     return Tensor(data=data, requires_grad=req_grad, depends_on=depends_on)
 
 
-def LeakyRelu(t: Tensor,leak=0.05) -> Tensor:
+def LeakyRelu(t: Tensor, leak=0.05) -> Tensor:
     """
-    fill 'data' and 'req_grad' and implement LeakyRelu grad_fn 
+    fill 'data' and 'req_grad' and implement LeakyRelu grad_fn
     hint: use np.where like Relu method but for LeakyRelu
     """
     # TODO: implement leaky_relu function
-    
-    data = ...
-    
-    req_grad = ...
+
+    data = np.where(t.data > 0, t.data, leak * t.data)
+    req_grad = t.requires_grad
+
     if req_grad:
         def grad_fn(grad: np.ndarray):
-            return None
-        
+            return np.where(t.data > 0, grad, leak * grad)
+
         depends_on = [Dependency(t, grad_fn)]
     else:
         depends_on = []

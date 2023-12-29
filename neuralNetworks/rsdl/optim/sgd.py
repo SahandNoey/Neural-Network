5950a8501c49afe8b03262a1fed693fe2ea94212
SahandNoey <sahandnoey7798@gmail.com>
2023-12-29 19:17:14 +0330

    SGD implemented

--- neuralNetworks/rsdl/optim/sgd.py
+++ neuralNetworks/rsdl/optim/sgd.py
@@ -1,14 +1,17 @@
 from rsdl.optim import Optimizer
 
 # TODO: implement step function
+
+
 class SGD(Optimizer):
-    def __init__(self, layers, learning_rate=0.1):
+    def __init__(self, layers, lr=0.1):
         super().__init__(layers)
-        self.learning_rate = learning_rate
+        self.lr = lr
 
     def step(self):
         # TODO: update weight and biases ( Don't use '-=' and use l.weight = l.weight - ... )
         for l in self.layers:
-            l.weight = ...
+            weights_and_bias = l.parameters()
+            l.weight = l.weight - self.lr * weights_and_bias[0].grad
             if l.need_bias:
-                l.bias = ...
+                l.bias = l.bias - self.lr * weights_and_bias[1].grad

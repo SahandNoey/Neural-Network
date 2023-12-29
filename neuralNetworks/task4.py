3ac55a08a19ac5873a99e9d7da6a2546c6b5b3b4
SahandNoey <sahandnoey7798@gmail.com>
2023-12-29 20:35:08 +0330

    task4 implemented

--- neuralNetworks/task4.py
+++ neuralNetworks/task4.py
@@ -9,39 +9,44 @@ coef = Tensor(np.array([-7, +3, -9]))
 y = X @ coef + 6
 
 # TODO: define w and b (y = w x + b) with random initialization ( you can use np.random.randn )
-w = ...
-b = ...
+w = Tensor(np.random.randn(3, 1), requires_grad=True)
+b = Tensor(np.random.randn(), requires_grad=True)
 
 print(w)
 print(b)
 
-learning_rate = ...
-batch_size = ...
+learning_rate = 0.1
+batch_size = 32
 
 for epoch in range(100):
-    
+
     epoch_loss = 0.0
-    
+
     for start in range(0, 100, batch_size):
         end = start + batch_size
 
         inputs = X[start:end]
 
         # TODO: predicted
-        predicted = ...
+        predicted = inputs@w+b
 
         actual = y[start:end]
         # TODO: calcualte MSE loss
-        
+        actual.data = actual.data.reshape(-1, 1)
+        inputs.zero_grad()
+        w.zero_grad()
+        b.zero_grad()
+
+        loss = loss_functions.MeanSquaredError(predicted, actual)
         # TODO: backward
         # hint you need to just do loss.backward()
-
-        epoch_loss += ...
+        loss.backward()
+        epoch_loss += loss
 
 
         # TODO: update w and b (Don't use 'w -= ' and use ' w = w - ...') (you don't need to use optim.SGD in this task)
-        w = ...
-        b = ...
+        w = w - learning_rate * w.grad
+        b = b - learning_rate * b.grad
 
 print(w)
 print(b)
